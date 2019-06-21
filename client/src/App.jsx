a6a4cdd384e27984aa56b8618a3a67e2148312ef
abimaelhernandez <hernandezabimael364@gmail.com>
2019-06-21 13:22:01 -0500

    restruturing the app

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,9 +1,14 @@
 import React, { Component } from 'react';
 
 export default class App extends Component {
+
+  // <img src="../imageFile/cuteHat.jpeg"  alt="" height="200" width="350"/><br/>
+
   render() {
     return (
-      <div> hola </div>
-    );
+      <div className="App">
+          <h1> What is your next move </h1>
+      </div>
+    )
   }
 }
